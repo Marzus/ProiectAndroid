6342dcdb0b271840c7ebe0e97d837bef4f50d9a8
Marzus <marius.mdumitrache@yahoo.ro>
2016-12-01 17:27:46 +0200

    Creare custom adaptor pt liste expandabile

--- app/src/main/java/com/proiect/echipa478a/proiectandroid/ui/MainActivity.java
+++ app/src/main/java/com/proiect/echipa478a/proiectandroid/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.proiect.echipa478a.proiectandroid;
+package com.proiect.echipa478a.proiectandroid.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.proiect.echipa478a.proiectandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 

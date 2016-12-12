c38a1afbd9c92a7c3ebee0af8eb7eacd44a66273
Marzus <marius.mdumitrache@yahoo.ro>
2016-12-12 20:42:49 +0200

    Creare lista iteme de bidding cu imagine, nume, pret si button de bid.

--- app/src/main/java/com/proiect/echipa478a/proiectandroid/ui/MainActivity.java
+++ app/src/main/java/com/proiect/echipa478a/proiectandroid/ui/MainActivity.java
@@ -2,18 +2,33 @@ package com.proiect.echipa478a.proiectandroid.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.proiect.echipa478a.proiectandroid.R;
+import com.proiect.echipa478a.proiectandroid.custom.adapters.SubitemsListAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView test = (TextView) findViewById(R.id.testView);
-        test.setText("Test Branch");
-        test.setText("Test Branch2");
+
+        itemsListView = (ListView) findViewById(R.id.itemsListView);
+
+        Map<String, Double> dummyItems = new HashMap<>();
+
+        dummyItems.put("item 1", 2.18);
+        dummyItems.put("item 2", 3.10);
+        dummyItems.put("item 3", 34D);
+
+        SubitemsListAdapter<String> itemsArrayAdapter = new SubitemsListAdapter<>(this, dummyItems);
+
+        itemsListView.setAdapter(itemsArrayAdapter);
     }
 }
